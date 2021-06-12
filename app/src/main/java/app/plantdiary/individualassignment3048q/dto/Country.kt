c6d2a5c6358f2411fec2652e629476a490397b4e
Erich Wagner <erswagner@gmail.com>
2021-06-12 12:24:24 -0400

    Making tests pass3

--- app/src/main/java/app/plantdiary/individualassignment3048q/dto/Country.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/dto/Country.kt
@@ -2,6 +2,6 @@ package app.plantdiary.individualassignment3048q.dto
 
 data class Country(var code: String, var name: String) {
     override fun toString(): String {
-        return name
+        return name + " " + code
     }
 }

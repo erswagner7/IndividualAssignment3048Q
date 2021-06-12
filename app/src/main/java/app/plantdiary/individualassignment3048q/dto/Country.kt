e7ca15e62fdc288d212650c8b43f2b9ce887891b
Erich Wagner <erswagner@gmail.com>
2021-06-12 11:07:45 -0400

    Changes made to DTO

--- app/src/main/java/app/plantdiary/individualassignment3048q/dto/Country.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/dto/Country.kt
@@ -1,6 +1,6 @@
 package app.plantdiary.individualassignment3048q.dto
 
-data class Country(var countries: String, var code: String, var name: String) {
+data class Country(var code: String, var name: String) {
     override fun toString(): String {
         return name
     }
